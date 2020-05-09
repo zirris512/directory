451050641ff08dd27dab7859a3cc9e2f43449cbb
Brent <mina512@live.com>
2020-05-08 22:06:12 -0500

    Updates

---
+++ precache-manifest.c223f89738a9f7152441b53476cf8a90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd1c3ac507c0dbca90cd2149e18f8cc6",
+    "url": "/directory/index.html"
+  },
+  {
+    "revision": "ac59b62b8bbe45b00a01",
+    "url": "/directory/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "d13f676be6343da8013f",
+    "url": "/directory/static/js/2.e01f3014.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/directory/static/js/2.e01f3014.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac59b62b8bbe45b00a01",
+    "url": "/directory/static/js/main.bce8c2d4.chunk.js"
+  },
+  {
+    "revision": "0190b0ad236a8f52f0e4",
+    "url": "/directory/static/js/runtime-main.a6c4fa97.js"
+  }
+]);
